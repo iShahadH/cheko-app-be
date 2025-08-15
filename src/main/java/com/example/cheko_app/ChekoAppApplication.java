0e93942146289adef7f9df9b8397c1db0ae16371
salohaydib <salohaydib@tahakom.com>
2025-08-15 18:07:08 +0300

    init project

---
+++ src/main/java/com/example/cheko_app/ChekoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheko_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChekoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChekoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cheko_app/ChekoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheko_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChekoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
